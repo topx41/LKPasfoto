8745a8c29ab1b10f485499751596928a0c77494f
topx41 <141309906+topx41@users.noreply.github.com>
2026-07-28 20:02:20 +0700

    add android
    add android

---
+++ android/app/src/main/java/com/lkpassfoto/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lkpassfoto.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lkpassfoto.app',
+  appName: 'LK Pasfoto',
+  webDir: 'dist'
+};
+
+export default config;
